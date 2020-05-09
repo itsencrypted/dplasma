cba7d7fde157fb5c2a435729a437364794f59b8d
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-05-09 15:47:08 -0300

    Update blood_types.dart

--- lib/models/blood_types.dart
+++ lib/models/blood_types.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 enum BloodTypes {
   A_Plus,
   A_Minus,
@@ -52,3 +54,44 @@ enum BloodTypes {
 //1	NYUpatient1 (B-)
 //1	NYUpatient2 (AB+)
 //1	MtSinaipatient3 (A-)
+
+Widget getDropdownBloodTypesList() {
+  var dropdownList = [
+    DropdownMenuItem(
+      value: 'select',
+      child: Text('Please select your role:'),
+    ),
+    DropdownMenuItem(
+      value: "0",
+      child: Text("A+"),
+    ),
+    DropdownMenuItem(
+      value: "1",
+      child: Text("A-"),
+    ),
+    DropdownMenuItem(
+      value: "2",
+      child: Text("B+"),
+    ),
+    DropdownMenuItem(
+      value: "3",
+      child: Text("B-"),
+    ),
+    DropdownMenuItem(
+      value: "4",
+      child: Text("AB+"),
+    ),
+    DropdownMenuItem(
+      value: "5",
+      child: Text("AB-"),
+    ),
+    DropdownMenuItem(
+      value: "6",
+      child: Text("O+"),
+    ),
+    DropdownMenuItem(
+      value: "7",
+      child: Text("O-"),
+    )
+  ];
+}
