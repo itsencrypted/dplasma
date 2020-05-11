318ee172eacbcb4c242e2714620f569f8d67dc93
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-05-11 15:02:59 -0300

    Requesting blood Hematologist

--- lib/screens/action_mtsinaihospital_screen.dart
+++ lib/screens/action_mtsinaihospital_screen.dart
@@ -1,8 +1,10 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:dplasma/constants.dart';
 import 'package:dplasma/screens/login_hematologist_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:dplasma/models/registration_form.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MtSinaiHospitalActionScreen extends StatefulWidget {
   static const String id = 'MtSinaihospitalAction_screen';
@@ -12,118 +14,233 @@ class MtSinaiHospitalActionScreen extends StatefulWidget {
       _MtSinaiHospitalActionScreenState();
 }
 
-class _MtSinaiHospitalActionScreenState extends
-State<MtSinaiHospitalActionScreen> {
+class _MtSinaiHospitalActionScreenState
+    extends State<MtSinaiHospitalActionScreen> {
+  SharedPreferences prefs;
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () async {
+      prefs = await SharedPreferences.getInstance();
+    });
+  }
+
+  showSucessMessage(msg) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+              // title: Text('Good'),
+              content: Text(
+                msg,
+                style: GoogleFonts.pressStart2p(
+                  textStyle: TextStyle(
+                    fontSize: 14.0,
+                  ),
+                ),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    })
+              ]);
+        });
+  }
+
+  showInfoMessage(msg) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+              // title: Text('Good'),
+              content: Text(
+                msg,
+                style: GoogleFonts.pressStart2p(
+                  textStyle: TextStyle(
+                    fontSize: 14.0,
+                  ),
+                ),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    })
+              ]);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-          backgroundColor: Colors.black,
-          body: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            PersonaActionAvatar(
+              personaImage: ('assets/images/mtsinaihospital.jpg'),
+              onPressed: () {
+                Navigator.pushReplacementNamed(
+                    context, HematologistLoginScreen.id);
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+                'Mt Sinai Hospital\n'
+                '\n'
+                'Main Directory',
+                style: hospitalFont),
+            SizedBox(
+              height: 50,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                PersonaActionAvatar(
-                  personaImage: ('assets/images/mtsinaihospital.jpg'),
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, HematologistLoginScreen.id);
-                  },
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text('Mt Sinai Hospital\n'
-                    '\n'
-                    'Main Directory', style: hospitalFont),
-                SizedBox(height: 50,),
+                Text('A -',
+                    style: GoogleFonts.pressStart2p(
+                        textStyle: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.yellow,
+                    ))),
+                Text('Request quantity:[1]',
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.pressStart2p(
+                        textStyle:
+                            TextStyle(fontSize: 16.0, color: Colors.yellow))),
+                SizedBox(height: 30),
                 Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
-                    Text('A -',
-                        style: GoogleFonts
-                            .pressStart2p(
-                            textStyle: TextStyle(
-                              fontSize: 16.0,
-                              color: Colors.yellow,))),
-                    Text('Request quantity:[1]', textAlign: TextAlign.left,
-                        style: GoogleFonts
-                            .pressStart2p(
-                            textStyle: TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.yellow))),
-                    SizedBox(height: 30),
-                    Column(
+                    Text('Convalescent Plasma Program_', style: hospitalFont),
+                    Row(
                       children: <Widget>[
-                        Text('Convalescent Plasma Program_', style:
-                        hospitalFont),
-                        Row(
+                        ButtonBar(
                           children: <Widget>[
-                            ButtonBar(
-                              children: <Widget>[
-                                SizedBox(width: 50,),
-                                FlatButton(onPressed: () {},
-                                  child: Text('A+', style: smallhospitalFont,),
-                                  color: Colors.white54,),
-                                FlatButton(onPressed: () {
-                                  //TODO: execute this button. ONLY
-                                  // HEMATOLOGIST CAN CLICK. No modern pop up
-                                  // message. Use hospitalFonts (at constants
-                                  // .dart)
+                            SizedBox(
+                              width: 50,
+                            ),
+                            FlatButton(
+                              onPressed: () {},
+                              child: Text(
+                                'A+',
+                                style: smallhospitalFont,
+                              ),
+                              color: Colors.white54,
+                            ),
+                            FlatButton(
+                              onPressed: () {
+                                //TODO: execute this button. ONLY
+                                // HEMATOLOGIST CAN CLICK. No modern pop up
+                                // message. Use hospitalFonts (at constants
+                                // .dart)
 //                                  "yes, NY Blood Bank is sending you a A- "
 //                                      "plasma bag",
-                                },
-                                  child: Text('A-', style: smallhospitalFont,),
-                                  color: Colors.white54,),
-                                FlatButton(onPressed: () {},
-                                  child: Text('B+', style: smallhospitalFont,),
-                                  color: Colors.white54,),
-                                FlatButton(onPressed: () {},
-                                  child: Text('B-', style: smallhospitalFont,),
-                                  color: Colors.white54,),
-                              ],
+                                String role = prefs.getString('role');
+                                print(role);
+                                if (role == "Hematologist") {
+                                  showSucessMessage(
+                                      "Yes, NY Blood Bank is sending you a A- plasma bag");
+                                } else {
+                                  showInfoMessage(
+                                      "Sorry, you're not a Hematologist");
+                                }
+                              },
+                              child: Text(
+                                'A-',
+                                style: smallhospitalFont,
+                              ),
+                              color: Colors.white54,
+                            ),
+                            FlatButton(
+                              onPressed: () {},
+                              child: Text(
+                                'B+',
+                                style: smallhospitalFont,
+                              ),
+                              color: Colors.white54,
+                            ),
+                            FlatButton(
+                              onPressed: () {},
+                              child: Text(
+                                'B-',
+                                style: smallhospitalFont,
+                              ),
+                              color: Colors.white54,
                             ),
                           ],
                         ),
-                        Row(
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        ButtonBar(
                           children: <Widget>[
-                            ButtonBar(children: <Widget>[
-                              SizedBox(width: 50,),
-                              FlatButton(onPressed: () {},
-                                child: Text('AB+', style: smallhospitalFont,),
-                                color: Colors.white54,),
-                              FlatButton(onPressed: () {},
-                                child: Text('AB-', style: smallhospitalFont,),
-                                color: Colors.white54,),
-                              FlatButton(onPressed: () {},
-                                child: Text('O+', style: smallhospitalFont,),
-                                color: Colors.white54,),
-                              FlatButton(onPressed: () {},
-                                child: Text('O-', style: smallhospitalFont,),
-                                color: Colors.white54,),
-                            ],)
+                            SizedBox(
+                              width: 50,
+                            ),
+                            FlatButton(
+                              onPressed: () {},
+                              child: Text(
+                                'AB+',
+                                style: smallhospitalFont,
+                              ),
+                              color: Colors.white54,
+                            ),
+                            FlatButton(
+                              onPressed: () {},
+                              child: Text(
+                                'AB-',
+                                style: smallhospitalFont,
+                              ),
+                              color: Colors.white54,
+                            ),
+                            FlatButton(
+                              onPressed: () {},
+                              child: Text(
+                                'O+',
+                                style: smallhospitalFont,
+                              ),
+                              color: Colors.white54,
+                            ),
+                            FlatButton(
+                              onPressed: () {},
+                              child: Text(
+                                'O-',
+                                style: smallhospitalFont,
+                              ),
+                              color: Colors.white54,
+                            ),
                           ],
                         )
                       ],
-                    ),
-
-                    //button only visible to hematologist.
-                    Text('Press to Request', textAlign: TextAlign.left,
-                        style: GoogleFonts
-                            .pressStart2p(
-                            textStyle: TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.yellow))),
-                    SizedBox(height: 20,),
-                    Text('Provider: NY Blood Center', textAlign: TextAlign.left,
-                        style: GoogleFonts
-                            .pressStart2p(
-                            textStyle: TextStyle(
-                                fontSize: 12.0,
-                                color: Colors.blueAccent))),
+                    )
                   ],
                 ),
+
+                //button only visible to hematologist.
+                Text('Press to Request',
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.pressStart2p(
+                        textStyle:
+                            TextStyle(fontSize: 16.0, color: Colors.yellow))),
+                SizedBox(
+                  height: 20,
+                ),
+                Text('Provider: NY Blood Center',
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.pressStart2p(
+                        textStyle: TextStyle(
+                            fontSize: 12.0, color: Colors.blueAccent))),
               ],
             ),
-          ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/action_nyuhospital_screen.dart
+++ lib/screens/action_nyuhospital_screen.dart
@@ -1,8 +1,10 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:dplasma/constants.dart';
 import 'package:dplasma/screens/login_hematologist_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:dplasma/models/registration_form.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NYUHospitalActionScreen extends StatefulWidget {
   static const String id = 'NYUhospitalAction_screen';
@@ -13,9 +15,65 @@ class NYUHospitalActionScreen extends StatefulWidget {
 }
 
 class _NYUHospitalActionScreenState extends State<NYUHospitalActionScreen> {
+  SharedPreferences prefs;
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () async {
+      prefs = await SharedPreferences.getInstance();
+    });
+  }
+
+ showSucessMessage(msg) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+              // title: Text('Good'),
+              content: Text(
+                msg,
+                style: GoogleFonts.pressStart2p(
+                  textStyle: TextStyle(
+                    fontSize: 14.0,
+                  ),
+                ),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    })
+              ]);
+        });
+  }
+
+  showInfoMessage(msg) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+              // title: Text('Good'),
+              content: Text(
+                msg,
+                style: GoogleFonts.pressStart2p(
+                  textStyle: TextStyle(
+                    fontSize: 14.0,
+                  ),
+                ),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    })
+              ]);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
@@ -24,114 +82,177 @@ class _NYUHospitalActionScreenState extends State<NYUHospitalActionScreen> {
           children: <Widget>[
             PersonaActionAvatar(
               personaImage: ('assets/images/nyuhospital.jpg'),
-              onPressed: (){
-                Navigator.pushReplacementNamed(context, HematologistLoginScreen.id);
-                },
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
             SizedBox(
               height: 20,
             ),
-            Text('NYU Hospital\n'
+            Text(
+                'NYU Hospital\n'
                 '\n'
-                'Main Directory', style: hospitalFont),
-            SizedBox(height: 50,),
+                'Main Directory',
+                style: hospitalFont),
+            SizedBox(
+              height: 50,
+            ),
             Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                Text( 'B -',
-                    style: GoogleFonts
-                        .pressStart2p(
+                Text('B -',
+                    style: GoogleFonts.pressStart2p(
                         textStyle: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.yellow,))),
-                Text( 'Request quantity:[1]',  textAlign: TextAlign.left,
-                    style: GoogleFonts
-                        .pressStart2p(
-                        textStyle: TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.yellow))),
-                SizedBox(height: 30,),
-                Text( 'AB +',  textAlign: TextAlign.left,
-                    style: GoogleFonts
-                        .pressStart2p(
-                        textStyle: TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.yellow))),
-                Text( 'Request quantity:[1]',  textAlign: TextAlign.left,
-                    style: GoogleFonts
-                        .pressStart2p(
-                        textStyle: TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.yellow))),
+                      fontSize: 16.0,
+                      color: Colors.yellow,
+                    ))),
+                Text('Request quantity:[1]',
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.pressStart2p(
+                        textStyle:
+                            TextStyle(fontSize: 16.0, color: Colors.yellow))),
+                SizedBox(
+                  height: 30,
+                ),
+                Text('AB +',
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.pressStart2p(
+                        textStyle:
+                            TextStyle(fontSize: 16.0, color: Colors.yellow))),
+                Text('Request quantity:[1]',
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.pressStart2p(
+                        textStyle:
+                            TextStyle(fontSize: 16.0, color: Colors.yellow))),
               ],
             ),
-
-            SizedBox(height:30),
+            SizedBox(height: 30),
             Column(
               children: <Widget>[
-                Text( 'Convalescent Plasma Program_', style:
-                hospitalFont),
+                Text('Convalescent Plasma Program_', style: hospitalFont),
                 Row(
                   children: <Widget>[
                     ButtonBar(
                       children: <Widget>[
-                        SizedBox(width: 50,),
-                        FlatButton(onPressed: (){},
-                          child: Text('A+', style: smallhospitalFont,),
-                          color: Colors.white54,),
-                        FlatButton(onPressed: (){},
-                          child: Text('A-', style: smallhospitalFont,),
-                          color: Colors.white54,),
-                        FlatButton(onPressed: (){},
-                          child: Text('B+', style: smallhospitalFont,),
-                          color: Colors.white54,),
-                        FlatButton(onPressed: (){
-                          //TODO: Execute this action
+                        SizedBox(
+                          width: 50,
+                        ),
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text(
+                            'A+',
+                            style: smallhospitalFont,
+                          ),
+                          color: Colors.white54,
+                        ),
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text(
+                            'A-',
+                            style: smallhospitalFont,
+                          ),
+                          color: Colors.white54,
+                        ),
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text(
+                            'B+',
+                            style: smallhospitalFont,
+                          ),
+                          color: Colors.white54,
+                        ),
+                        FlatButton(
+                          onPressed: () {
+                            //TODO: Execute this action
 //                          "yes, NY Blood Bank is sending you a B- plasma bag",
-                        },
-                          child: Text('B-', style: smallhospitalFont,),
-                          color: Colors.white54,),
+                            String role = prefs.getString('role');
+                            if (role == "Hematologist") {
+                              showSucessMessage(
+                                  "Yes, NY Blood Bank is sending you a B- plasma bag");
+                            } else {
+                              showInfoMessage(
+                                  "Sorry, you're not a Hematologist");
+                            }
+                          },
+                          child: Text(
+                            'B-',
+                            style: smallhospitalFont,
+                          ),
+                          color: Colors.white54,
+                        ),
                       ],
                     ),
                   ],
                 ),
                 Row(
                   children: <Widget>[
-                    ButtonBar(children: <Widget>[
-                      SizedBox(width: 50,),
-                      FlatButton(onPressed: (){
-                        //TODO: Execute this action
+                    ButtonBar(
+                      children: <Widget>[
+                        SizedBox(
+                          width: 50,
+                        ),
+                        FlatButton(
+                          onPressed: () {
+                            //TODO: Execute this action
 //                        'unfortunately, no AB+ plasma bags available today'
-                      },
-                        child: Text('AB+', style: smallhospitalFont,),
-                        color: Colors.white54,),
-                      FlatButton(onPressed: (){},
-                        child: Text('AB-', style: smallhospitalFont,),
-                        color: Colors.white54,),
-                      FlatButton(onPressed: (){},
-                        child: Text('O+', style: smallhospitalFont,),
-                        color: Colors.white54,),
-                      FlatButton(onPressed: (){},
-                        child: Text('O-', style: smallhospitalFont,),
-                        color: Colors.white54,),
-                    ],)
+                            String role = prefs.getString('role');
+                            if (role == "Hematologist") {
+                              showSucessMessage(
+                                  "Sorry, unfortunately there's no AB+ plasma bag available");
+                            } else {
+                              showInfoMessage(
+                                  "Sorry, you're not a Hematologist");
+                            }
+                          },
+                          child: Text(
+                            'AB+',
+                            style: smallhospitalFont,
+                          ),
+                          color: Colors.white54,
+                        ),
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text(
+                            'AB-',
+                            style: smallhospitalFont,
+                          ),
+                          color: Colors.white54,
+                        ),
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text(
+                            'O+',
+                            style: smallhospitalFont,
+                          ),
+                          color: Colors.white54,
+                        ),
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text(
+                            'O-',
+                            style: smallhospitalFont,
+                          ),
+                          color: Colors.white54,
+                        ),
+                      ],
+                    )
                   ],
                 )
               ],
             ),
-            Text( 'Press to Request',  textAlign: TextAlign.left,
-                style: GoogleFonts
-                    .pressStart2p(
-                    textStyle: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.yellow))),
-            SizedBox(height: 20,),
-            Text( 'Provider: NY Blood Center',  textAlign: TextAlign.left,
-                style: GoogleFonts
-                    .pressStart2p(
-                    textStyle: TextStyle(
-                        fontSize: 12.0,
-                        color: Colors.blueAccent))),
+            Text('Press to Request',
+                textAlign: TextAlign.left,
+                style: GoogleFonts.pressStart2p(
+                    textStyle:
+                        TextStyle(fontSize: 16.0, color: Colors.yellow))),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Provider: NY Blood Center',
+                textAlign: TextAlign.left,
+                style: GoogleFonts.pressStart2p(
+                    textStyle:
+                        TextStyle(fontSize: 12.0, color: Colors.blueAccent))),
           ],
         ),
       ),
